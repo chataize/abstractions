29ff0429d16b27b6dff2c3dd32eb60bbe6c18db2
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-11-03 01:55:49 +0100

    Fix ISettingsButton interface missing Style property

--- src/Settings/ISettingsButton.cs
+++ src/Settings/ISettingsButton.cs
@@ -2,5 +2,7 @@ namespace ChatAIze.Abstractions.Settings;
 
 public interface ISettingsButton : IPluginSetting
 {
+    public ButtonStyle Style { get; }
+
     public Func<ValueTask> Callback { get; }
 }
