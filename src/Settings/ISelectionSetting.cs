6d7391c8b897132d31b69f93ac1ed0d535f96ad7
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-11-04 22:13:32 +0100

    Add DefaultValue property to ISelectionSetting interface

--- src/Settings/ISelectionSetting.cs
+++ src/Settings/ISelectionSetting.cs
@@ -5,4 +5,6 @@ public interface ISelectionSetting<T> : IEditableSetting
     public SelectionSettingStyle Style { get; }
 
     public ICollection<ISelectionChoice<T>> Choices { get; }
+
+    public T DefaultValue { get; }
 }
