c95b99788c4b356be046fc61f4b5cee9d6b0bccf
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-11-07 00:42:26 +0100

    Add ItemPlaceholder property to IListSetting interface

--- src/Settings/IListSetting.cs
+++ src/Settings/IListSetting.cs
@@ -2,6 +2,8 @@ namespace ChatAIze.Abstractions.Settings;
 
 public interface IListSetting : IEditableSetting
 {
+    public string ItemPlaceholder { get; }
+
     public int MaxItems { get; }
 
     public int MaxItemLength { get; }
