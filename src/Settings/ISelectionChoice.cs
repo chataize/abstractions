2ae42571ed102adad726a4b41cd542a3b7f83396
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-11-07 23:46:02 +0100

    Make Title property nullable

--- src/Settings/ISelectionChoice.cs
+++ src/Settings/ISelectionChoice.cs
@@ -2,7 +2,7 @@ namespace ChatAIze.Abstractions.Settings;
 
 public interface ISelectionChoice
 {
-    public string Title { get; }
+    public string? Title { get; }
 
     public string Value { get; }
 
