b6b2dbe42b0d237445bfc4dc21e99fed87e3a269
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-11-09 14:06:59 +0100

    Add cancellationToken parameter to GetAsync and SetAsync methods

--- src/Settings/IPluginSettings.cs
+++ src/Settings/IPluginSettings.cs
@@ -2,7 +2,7 @@ namespace ChatAIze.Abstractions.Settings;
 
 public interface IPluginSettings
 {
-    public ValueTask<T?> GetAsync<T>(string key);
+    public ValueTask<T?> GetAsync<T>(string key, CancellationToken cancellationToken = default);
 
-    public ValueTask SetAsync<T>(string key, T? value);
+    public ValueTask SetAsync<T>(string key, T? value, CancellationToken cancellationToken = default);
 }
