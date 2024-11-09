a156968b4684f11b3dcb4f958295f8acb706f06c
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-11-09 01:48:10 +0100

    Pass plugin settings to LoadAsync method

--- src/Plugins/IPluginLoader.cs
+++ src/Plugins/IPluginLoader.cs
@@ -1,6 +1,8 @@
+using ChatAIze.Abstractions.Settings;
+
 namespace ChatAIze.Abstractions.Plugins;
 
 public interface IPluginLoader
 {
-    public ValueTask<IChatbotPlugin> LoadAsync(CancellationToken cancellationToken = default);
+    public ValueTask<IChatbotPlugin> LoadAsync(IPluginSettings settings, CancellationToken cancellationToken = default);
 }
