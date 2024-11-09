34f1196d7a1406139c42f108c737c55daade8214
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-11-09 02:10:10 +0100

    Remove settings parameter from LoadAsync method

--- src/Plugins/IPluginLoader.cs
+++ src/Plugins/IPluginLoader.cs
@@ -1,8 +1,6 @@
-using ChatAIze.Abstractions.Settings;
-
 namespace ChatAIze.Abstractions.Plugins;
 
 public interface IPluginLoader
 {
-    public ValueTask<IChatbotPlugin> LoadAsync(IPluginSettings settings, CancellationToken cancellationToken = default);
+    public ValueTask<IChatbotPlugin> LoadAsync(CancellationToken cancellationToken = default);
 }
