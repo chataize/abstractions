160561bae72f974af2f91afadd7bb33894a7094d
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-11-10 17:52:40 +0100

    Add ShowCaptchaAsync method to IExecutionContext interface

--- src/Chat/IExecutionContext.cs
+++ src/Chat/IExecutionContext.cs
@@ -20,5 +20,7 @@ public interface IExecutionContext
 
     public ValueTask<bool> ShowConfirmationAsync(string title, string message, string yesText = "Yes", string noText = "No", CancellationToken cancellationToken = default);
 
+    public ValueTask<bool> ShowCaptchaAsync(CancellationToken cancellationToken = default);
+
     public ValueTask<bool> VerifyEmailAsync(string email, CancellationToken cancellationToken = default);
 }
