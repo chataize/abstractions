6a386d347293344e6c6876653573423c75c0686b
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-11-11 17:34:28 +0100

    Add IFunctionAction interface

---
+++ src/Actions/IFunctionAction.cs
@@ -0,0 +1,14 @@
+using ChatAIze.Abstractions.Settings;
+
+namespace ChatAIze.Abstractions.Actions;
+
+public interface IFunctionAction
+{
+    public string Key { get; }
+
+    public string Name { get; }
+
+    public ICollection<IPluginSetting> Settings { get; }
+
+    public Func<IDictionary<string, object>, ValueTask<object>> Callback { get; }
+}
