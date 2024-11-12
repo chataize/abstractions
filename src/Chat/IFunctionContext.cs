9e858b6c9ae38ebe1421a88ccce0e22dd4c7fcdd
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-11-12 16:16:26 +0100

    Update IFunctionContext to inherit from IActionContext

--- src/Chat/IFunctionContext.cs
+++ src/Chat/IFunctionContext.cs
@@ -1,34 +1,9 @@
-using ChatAIze.Abstractions.Plugins;
-using ChatAIze.Abstractions.Settings;
+using ChatAIze.Abstractions.Actions;
 
 namespace ChatAIze.Abstractions.Chat;
 
-public interface IFunctionContext
+public interface IFunctionContext : IActionContext
 {
-    public IPluginSettings Settings { get; }
-
-    public Guid ChatId { get; }
-
-    public Guid UserId { get; }
-
-    public string? UserName { get; }
-
-    public string? UserEmail { get; }
-
-    public string? UserIpAddress { get; }
-
-    public bool IsPreview { get; }
-
-    public bool IsDebugModeOn { get; }
-
-    public bool IsCommunicationSandboxed { get; }
-
-    public T? GetPlugin<T>(string? id = null) where T : IChatbotPlugin;
-
-    public ValueTask<string?> GetUserPropertyAsync(string key, CancellationToken cancellationToken = default);
-
-    public ValueTask SetUserPropertyAsync(string key, string value, CancellationToken cancellationToken = default);
-
     public ValueTask<bool> ShowConfirmationAsync(string title, string message, string yesText = "Yes", string noText = "No", CancellationToken cancellationToken = default);
 
     public ValueTask<bool> ShowCaptchaAsync(CancellationToken cancellationToken = default);
