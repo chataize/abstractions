c9d357984409528c74a39457771375914dc9af0a
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-11-13 22:29:53 +0100

    Add IConditionContext interface

--- src/Chat/IActionContext.cs
+++ src/Chat/IActionContext.cs
@@ -1,30 +1,6 @@
-using ChatAIze.Abstractions.Plugins;
-
 namespace ChatAIze.Abstractions.Chat;
 
-public interface IActionContext
+public interface IActionContext : IConditionContext
 {
-    public IPluginSettings Settings { get; }
-
-    public Guid ChatId { get; }
-
-    public Guid UserId { get; }
-
-    public string? UserName { get; }
-
-    public string? UserEmail { get; }
-
-    public string? UserIpAddress { get; }
-
-    public bool IsPreview { get; }
-
-    public bool IsDebugModeOn { get; }
-
-    public bool IsCommunicationSandboxed { get; }
-
-    public T? GetPlugin<T>(string? id = null) where T : IChatbotPlugin;
-
-    public ValueTask<T> GetUserPropertyAsync<T>(string id, T defaultValue, CancellationToken cancellationToken = default);
-
     public ValueTask SetUserPropertyAsync<T>(string id, T? value, CancellationToken cancellationToken = default);
 }
---
+++ src/Chat/IConditionContext.cs
@@ -0,0 +1,28 @@
+using ChatAIze.Abstractions.Plugins;
+
+namespace ChatAIze.Abstractions.Chat;
+
+public interface IConditionContext
+{
+    public IPluginSettings Settings { get; }
+
+    public Guid ChatId { get; }
+
+    public Guid UserId { get; }
+
+    public string? UserName { get; }
+
+    public string? UserEmail { get; }
+
+    public string? UserIpAddress { get; }
+
+    public bool IsPreview { get; }
+
+    public bool IsDebugModeOn { get; }
+
+    public bool IsCommunicationSandboxed { get; }
+
+    public T? GetPlugin<T>(string? id = null) where T : IChatbotPlugin;
+
+    public ValueTask<T> GetUserPropertyAsync<T>(string id, T defaultValue, CancellationToken cancellationToken = default);
+}
