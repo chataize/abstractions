0e1373b2b6c31bee01b43d1dd5712581a88501b0
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-11-13 21:52:48 +0100

    Remove SetAsync method from IPluginSettings interface

--- src/Plugins/IPluginSettings.cs
+++ src/Plugins/IPluginSettings.cs
@@ -3,6 +3,4 @@ namespace ChatAIze.Abstractions.Plugins;
 public interface IPluginSettings
 {
     public ValueTask<T> GetAsync<T>(string id, T defaultValue, CancellationToken cancellationToken = default);
-
-    public ValueTask SetAsync<T>(string id, T? value, CancellationToken cancellationToken = default);
 }
