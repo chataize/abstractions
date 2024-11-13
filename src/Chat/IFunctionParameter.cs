c04254e1de59020db05de1d841cae5ec08a557fc
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-11-13 22:43:34 +0100

    Remove setters from IFunctionParameter interface properties

--- src/Chat/IFunctionParameter.cs
+++ src/Chat/IFunctionParameter.cs
@@ -2,13 +2,13 @@ namespace ChatAIze.Abstractions.Chat;
 
 public interface IFunctionParameter
 {
-    public string Name { get; set; }
+    public string Name { get; }
 
-    public string? Description { get; set; }
+    public string? Description { get; }
 
-    public Type Type { get; set; }
+    public Type Type { get; }
 
-    public bool IsRequired { get; set; }
+    public bool IsRequired { get; }
 
-    public ICollection<string> EnumValues { get; set; }
+    public ICollection<string> EnumValues { get; }
 }
