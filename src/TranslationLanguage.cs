a0629b83f98b90a847453832a71e56f9f34e3001
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-11-17 00:09:02 +0100

    Add TranslationLanguage enum

---
+++ src/TranslationLanguage.cs
@@ -0,0 +1,90 @@
+namespace ChatAIze.Abstractions;
+
+public enum TranslationLanguage
+{
+    Unspecified,
+
+    Arabic,
+
+    Catalan,
+
+    ChineseHongKongTraditional,
+
+    ChineseSimplified,
+
+    ChineseTraditional,
+
+    Croatian,
+
+    Czech,
+
+    Danish,
+
+    Dutch,
+
+    English,
+
+    EnglishCanada,
+
+    EnglishAustralia,
+
+    EnglishIndia,
+
+    EnglishUK,
+
+    EnglishUS,
+
+    Finnish,
+
+    French,
+
+    FrenchCanada,
+
+    German,
+
+    Greek,
+
+    Hebrew,
+
+    Hindi,
+
+    Hungarian,
+
+    Indonesian,
+
+    Italian,
+
+    Japanese,
+
+    Korean,
+
+    Malay,
+
+    NorwegianBokmal,
+
+    Polish,
+
+    PortugueseBrazil,
+
+    PortuguesePortugal,
+
+    Romanian,
+
+    Russian,
+
+    Slovak,
+
+    Spanish,
+
+    SpanishMexico,
+
+    Swedish,
+
+    Thai,
+
+    Turkish,
+
+    Ukrainian,
+
+    Vietnamese
+}
