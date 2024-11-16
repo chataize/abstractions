9d660dc9b66ae221370eac017dfb88720d52b2de
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-11-16 22:04:05 +0100

    Add SetQuickReplies method to IActionContext interface

--- src/Chat/IActionContext.cs
+++ src/Chat/IActionContext.cs
@@ -2,5 +2,7 @@ namespace ChatAIze.Abstractions.Chat;
 
 public interface IActionContext : IConditionContext
 {
+    public void SetQuickReplies(params IReadOnlyCollection<IQuickReply> quickReplies);
+
     public ValueTask SetUserPropertyAsync<T>(string id, T? value, CancellationToken cancellationToken = default);
 }
