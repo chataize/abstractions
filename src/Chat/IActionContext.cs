9bbff8ed58338951d25b5feb012efc319fcde7ff
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-11-16 22:23:09 +0100

    Add ShowFormAsync method to IActionContext interface

--- src/Chat/IActionContext.cs
+++ src/Chat/IActionContext.cs
@@ -1,14 +1,18 @@
+using ChatAIze.Abstractions.Settings;
+
 namespace ChatAIze.Abstractions.Chat;
 
 public interface IActionContext : IConditionContext
 {
     public void SetQuickReplies(params IReadOnlyCollection<IQuickReply> quickReplies);
 
-    public ValueTask<bool> ShowConfirmationAsync(string title, string message, string yesText = "Yes", string noText = "No", CancellationToken cancellationToken = default);
+    public ValueTask<bool> VerifyEmailAsync(string email, CancellationToken cancellationToken = default);
 
     public ValueTask<bool> ShowCaptchaAsync(CancellationToken cancellationToken = default);
 
-    public ValueTask<bool> VerifyEmailAsync(string email, CancellationToken cancellationToken = default);
+    public ValueTask<bool> ShowConfirmationAsync(string title, string message, string yesText = "Yes", string noText = "No", CancellationToken cancellationToken = default);
+
+    public ValueTask<(bool, IDictionary<string, object>)> ShowFormAsync(string title, string confirmText = "Confirm", string cancelText = "Cancel", CancellationToken cancellationToken = default, params IReadOnlyCollection<ISetting> settings);
 
     public ValueTask SetUserPropertyAsync<T>(string id, T? value, CancellationToken cancellationToken = default);
 }
