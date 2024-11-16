28673da402c761b2d11758a03bf9a5ecf077957a
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-11-16 23:00:50 +0100

    Make value parameter non-nullable

--- src/Chat/IActionContext.cs
+++ src/Chat/IActionContext.cs
@@ -4,7 +4,7 @@ public interface IActionContext : IFunctionContext
 {
     public IReadOnlyCollection<IActionResult> Results { get; }
 
-    public void SetPlaceholder(string id, object? value);
+    public void SetPlaceholder(string id, object value);
 
     public void SetSuccess(bool isSuccess);
 }
