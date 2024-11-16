6583cd45f83f7ee9281a17ff895ea0cf82fc55e1
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-11-17 00:02:27 +0100

    Add Description property to IFunctionAction interface

--- src/Chat/IFunctionAction.cs
+++ src/Chat/IFunctionAction.cs
@@ -8,6 +8,8 @@ public interface IFunctionAction : ISettingsContainer
 
     public string Title { get; }
 
+    public string? Description { get; }
+
     public Delegate Callback { get; }
 
     public IReadOnlyCollection<string> Placeholders { get; }
