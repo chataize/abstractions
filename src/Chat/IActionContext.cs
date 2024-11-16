42f7c034cca92c436146ae9a3f540e6b7bc3d119
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-11-16 22:06:45 +0100

    Move all IFunctionContext methods to IActionContext

--- src/Chat/IActionContext.cs
+++ src/Chat/IActionContext.cs
@@ -4,5 +4,11 @@ public interface IActionContext : IConditionContext
 {
     public void SetQuickReplies(params IReadOnlyCollection<IQuickReply> quickReplies);
 
+    public ValueTask<bool> ShowConfirmationAsync(string title, string message, string yesText = "Yes", string noText = "No", CancellationToken cancellationToken = default);
+
+    public ValueTask<bool> ShowCaptchaAsync(CancellationToken cancellationToken = default);
+
+    public ValueTask<bool> VerifyEmailAsync(string email, CancellationToken cancellationToken = default);
+
     public ValueTask SetUserPropertyAsync<T>(string id, T? value, CancellationToken cancellationToken = default);
 }
--- src/Chat/IFunctionContext.cs
+++ src/Chat/IFunctionContext.cs
@@ -1,10 +1,3 @@
 namespace ChatAIze.Abstractions.Chat;
 
-public interface IFunctionContext : IActionContext
-{
-    public ValueTask<bool> ShowConfirmationAsync(string title, string message, string yesText = "Yes", string noText = "No", CancellationToken cancellationToken = default);
-
-    public ValueTask<bool> ShowCaptchaAsync(CancellationToken cancellationToken = default);
-
-    public ValueTask<bool> VerifyEmailAsync(string email, CancellationToken cancellationToken = default);
-}
+public interface IFunctionContext : IActionContext { }
