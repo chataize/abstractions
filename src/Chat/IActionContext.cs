942cf683f3fa1e73883328b6ea5f7d8a15f80541
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-11-16 22:59:53 +0100

    Make IActionContext inherit from IFunctionContext

--- src/Chat/IActionContext.cs
+++ src/Chat/IActionContext.cs
@@ -1,26 +1,10 @@
-using ChatAIze.Abstractions.Settings;
-
 namespace ChatAIze.Abstractions.Chat;
 
-public interface IActionContext : IConditionContext
+public interface IActionContext : IFunctionContext
 {
     public IReadOnlyCollection<IActionResult> Results { get; }
 
-    public void SetStatus(string? status);
-
     public void SetPlaceholder(string id, object? value);
 
-    public void SetQuickReplies(params IReadOnlyCollection<IQuickReply> quickReplies);
-
     public void SetSuccess(bool isSuccess);
-
-    public ValueTask<bool> VerifyEmailAsync(string email, CancellationToken cancellationToken = default);
-
-    public ValueTask<bool> ShowCaptchaAsync(CancellationToken cancellationToken = default);
-
-    public ValueTask<bool> ShowConfirmationAsync(string title, string message, string yesText = "Yes", string noText = "No", CancellationToken cancellationToken = default);
-
-    public ValueTask<(bool, IDictionary<string, object>)> ShowFormAsync(string title, string confirmText = "Confirm", string cancelText = "Cancel", CancellationToken cancellationToken = default, params IReadOnlyCollection<ISetting> settings);
-
-    public ValueTask SetUserPropertyAsync<T>(string id, T? value, CancellationToken cancellationToken = default);
 }
--- src/Chat/IFunctionContext.cs
+++ src/Chat/IFunctionContext.cs
@@ -1,3 +1,20 @@
+using ChatAIze.Abstractions.Settings;
+
 namespace ChatAIze.Abstractions.Chat;
 
-public interface IFunctionContext : IActionContext { }
+public interface IFunctionContext : IConditionContext
+{
+    public void SetStatus(string? status);
+
+    public void SetQuickReplies(params IReadOnlyCollection<IQuickReply> quickReplies);
+
+    public ValueTask<bool> VerifyEmailAsync(string email, CancellationToken cancellationToken = default);
+
+    public ValueTask<bool> ShowCaptchaAsync(CancellationToken cancellationToken = default);
+
+    public ValueTask<bool> ShowConfirmationAsync(string title, string message, string yesText = "Yes", string noText = "No", CancellationToken cancellationToken = default);
+
+    public ValueTask<(bool, IDictionary<string, object>)> ShowFormAsync(string title, string confirmText = "Confirm", string cancelText = "Cancel", CancellationToken cancellationToken = default, params IReadOnlyCollection<ISetting> settings);
+
+    public ValueTask SetUserPropertyAsync<T>(string id, T? value, CancellationToken cancellationToken = default);
+}
