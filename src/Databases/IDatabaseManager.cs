efe6f7524398b3bc6f3a7db6e51e26bf654d6fd2
Marcel Kwiatkowski <marcel2215@icloud.com>
2025-01-07 16:21:14 +0100

    Add IDatabaseManager interface

---
+++ src/Databases/IDatabaseManager.cs
@@ -0,0 +1,42 @@
+namespace ChatAIze.Abstractions.Databases;
+
+public interface IDatabaseManager
+{
+    #region Databases
+
+    public Task<DatabaseCreationResult> CreateDatabaseAsync(string title, string? description = null);
+
+    public Task<IReadOnlyList<IDatabase>> GetAllDatabasesAsync();
+
+    public Task<IDatabase?> FindDatabaseAsync(Guid id);
+
+    public Task<IDatabase?> FindDatabaseAsync(string title);
+
+    public Task SetTitleAsync(IDatabase database, string title);
+
+    public Task SetDescriptionAsync(IDatabase database, string? description);
+
+    public Task DeleteAsync(IDatabase database);
+
+    #endregion
+
+    #region Items
+
+    public Task<ItemAdditionResult> AddAsync(IDatabase database, IDatabaseItem item);
+
+    public Task<IReadOnlyList<IDatabaseItem>> FindItemsAsync(IDatabase database, string? search = null, int count = 10, IDatabaseSorting? sorting = null, params IEnumerable<IDatabaseFilter> filters);
+
+    public Task<IDatabaseItem> FindFirstItemAsync(IDatabase database, string? search = null, params IEnumerable<IDatabaseFilter> filters);
+
+    public Task SetTitleAsync(IDatabaseItem item, string title);
+
+    public Task SetDescriptionAsync(IDatabaseItem item, string? description);
+
+    public Task SetPropertyAsync(IDatabaseItem item, string property, string? value);
+
+    public Task SetPropertiesAsync(IDatabaseItem item, IReadOnlyDictionary<string, string?> properties);
+
+    public Task DeleteAsync(IDatabaseItem item);
+
+    #endregion
+}
