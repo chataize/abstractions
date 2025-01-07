bb6baa56c9d398e407b70d3a38e1c3ad2c3506af
Marcel Kwiatkowski <marcel2215@icloud.com>
2025-01-07 14:48:29 +0100

    Add FilterType enum

---
+++ src/Databases/FilterType.cs
@@ -0,0 +1,28 @@
+namespace ChatAIze.Abstractions.Databases;
+
+public enum FilterType
+{
+    Equal,
+
+    NotEqual,
+
+    GreaterThan,
+
+    GreaterThanOrEqual,
+
+    LessThan,
+
+    LessThanOrEqual,
+
+    Contains,
+
+    NotContains,
+
+    StartsWith,
+
+    NotStartsWith,
+
+    EndsWith,
+
+    NotEndsWith
+}
