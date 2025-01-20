7fe1564da4d0353a3fb509a00753063b0607306f
Marcel Kwiatkowski <marcel2215@icloud.com>
2025-01-20 22:18:49 +0100

    Add IChatContext interface

---
+++ src/Chat/IChatContext.cs
@@ -0,0 +1,23 @@
+using ChatAIze.Abstractions.Databases;
+using ChatAIze.Abstractions.Plugins;
+
+namespace ChatAIze.Abstractions.Chat;
+
+public interface IChatContext
+{
+    public IPluginSettings Settings { get; }
+
+    public Guid ChatId { get; }
+
+    public IUserContext User { get; }
+
+    public IDatabaseManager Databases { get; }
+
+    public bool IsPreview { get; }
+
+    public bool IsDebugModeOn { get; }
+
+    public bool IsCommunicationSandboxed { get; }
+
+    public T? GetPlugin<T>(string? id = null) where T : IChatbotPlugin;
+}
--- src/Chat/IConditionContext.cs
+++ src/Chat/IConditionContext.cs
@@ -1,23 +1,3 @@
-using ChatAIze.Abstractions.Databases;
-using ChatAIze.Abstractions.Plugins;
-
 namespace ChatAIze.Abstractions.Chat;
 
-public interface IConditionContext
-{
-    public IPluginSettings Settings { get; }
-
-    public Guid ChatId { get; }
-
-    public IUserContext User { get; }
-
-    public IDatabaseManager Databases { get; }
-
-    public bool IsPreview { get; }
-
-    public bool IsDebugModeOn { get; }
-
-    public bool IsCommunicationSandboxed { get; }
-
-    public T? GetPlugin<T>(string? id = null) where T : IChatbotPlugin;
-}
+public interface IConditionContext : IChatContext { }
