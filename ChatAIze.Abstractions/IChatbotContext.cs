dd0f64debd6c0cf647060b0c272ac7d5be019309
Marcel Kwiatkowski <marcel2215@icloud.com>
2025-12-14 15:31:31 +0100

    Expose logging on chatbot context

--- ChatAIze.Abstractions/IChatbotContext.cs
+++ ChatAIze.Abstractions/IChatbotContext.cs
@@ -1,5 +1,7 @@
+using System;
 using ChatAIze.Abstractions.Databases;
 using ChatAIze.Abstractions.Plugins;
+using Microsoft.Extensions.Logging;
 
 namespace ChatAIze.Abstractions;
 
@@ -17,4 +19,12 @@ public interface IChatbotContext
     /// Gets the database manager used for interacting with data sources.
     /// </summary>
     public IDatabaseManager Databases { get; }
+
+    /// <summary>
+    /// Writes a log entry using the application's logging pipeline with the specified severity.
+    /// </summary>
+    /// <param name="level">The log level.</param>
+    /// <param name="message">The log message.</param>
+    /// <param name="exception">An optional exception.</param>
+    public void Log(LogLevel level, string message, Exception? exception = null);
 }
